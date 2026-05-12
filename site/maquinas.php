452da31ae2dfbce7c0155b9f4c29d4515358c3b3
Kferraz <karoline.ferraz23@gmail.com>
2026-05-12 11:59:40 -0300

    Implement CORS support in maquinas.php
    Added CORS headers and OPTIONS request handling.

--- site/maquinas.php
+++ site/maquinas.php
@@ -1,7 +1,15 @@
 <?php
-include __DIR__ . '/../conexao.php';
 
-header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit;
+}
+
+include __DIR__ . '/../conexao.php';
 
 $sql = "SELECT * FROM maquinas";
 $stmt = $pdo->query($sql);
