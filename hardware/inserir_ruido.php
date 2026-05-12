8312720e06f6714a56deee952980e03f21633174
Kferraz <karoline.ferraz23@gmail.com>
2026-05-12 11:57:50 -0300

    Add CORS headers to inserir_ruido.php

--- hardware/inserir_ruido.php
+++ hardware/inserir_ruido.php
@@ -1,7 +1,15 @@
 <?php
-include __DIR__ . '/../conexao.php';
 
-header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit;
+}
+
+include __DIR__ . '/../conexao.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
